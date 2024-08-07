8f45bc31ff74272faa5e9b442b68a65ec29c582a
unknown <datnguyen9184@gmail.com>
2024-08-07 21:18:59 +0700

    Latest update

--- scripts/copyPdfWorker.js
+++
@@ -1,8 +0,0 @@
-// scripts/copyPdfWorker.js
-import path from 'node:path';
-import fs from 'node:fs';
-
-const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
-const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.min.mjs');
-
-fs.copyFileSync(pdfWorkerPath, './public/pdf.worker.min.mjs');
